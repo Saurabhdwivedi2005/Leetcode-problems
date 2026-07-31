7019fd141c49f11581a840102bf34b46fbb1a88e
Saurabh Dwivedi <2k23.ece2311195@gmail.com>
2026-07-31 12:18:07 +0530

    Time: 6 ms (75.61%), Space: 25.4 MB (72.47%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int>mp(26,0);
+        int result=0;
+        for(char &ch:word){
+            mp[ch-'a']++;
+        
+        }
+        sort(begin(mp),end(mp), greater<int>());
+        for(int i=0;i<26;i++){
+            int freq=mp[i];
+            int press=i/8+1;
+            result+=freq*press;
+        }
+
+        
+        return result;
+        
+    }
+};
